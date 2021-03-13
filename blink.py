0fe8cd97f34ce39df019be2784bcc704a352273f
Marc Wintermantel <marc.wintermantel@gmail.com>
2021-03-13 20:23:35 +0000

    added progressive_jog

--- blink.py
+++ blink.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 import hal, time
 h = hal.component("blink")
-h.newpin("in", hal.HAL_BIT, hal.HAL_IN)
-h.newpin("out", hal.HAL_BIT, hal.HAL_OUT)
+h.newpin("auto", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("single", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("jog", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("is_running", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("is_paused", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("red", hal.HAL_BIT, hal.HAL_OUT)
+h.newpin("green", hal.HAL_BIT, hal.HAL_OUT)
 h.ready()
+
+ms_count = 0
+
 try:
     while 1:
-        if h['in']:
-          time.sleep(1)
-          h['out'] = 1
-          time.sleep(1)
-          h['out'] = 0
+        ms_count = int(time.clock()*1000)%1000
+
+        if (h['auto'] and h['is_running']):
+          h['green'] = True
+          h['red'] = False
+        elif (h['auto'] and h['is_paused']):
+          h['green'] = False
+          h['red'] = True
+        elif (h['single'] and ms_count < 500):
+          h['green'] = False
+          h['red'] = True
+        elif (h['single'] and ms_count > 500):
+          h['green'] = True
+          h['red'] = False
         else:
-          h['out'] = 0
+          h['green'] = False
+          h['red'] = False
+
+         
 except KeyboardInterrupt:
     raise SystemExit
---
+++ progressive_jog.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import hal, time
+h = hal.component("progressive_jog")
+h.newpin("count_in", hal.HAL_S32, hal.HAL_IN)
+h.newpin("delta_t", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("speed_scale", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("count_out", hal.HAL_S32, hal.HAL_OUT)
+h.newpin("speed", hal.HAL_FLOAT, hal.HAL_OUT)
+h.ready()
+
+speed = 0.0
+old_count = h['count_in']
+new_count = old_count
+old_time = time.time()
+new_time = old_time
+count_out = old_count
+
+try:
+    while 1:
+      new_time = time.time()
+      new_count = h['count_in']
+      while (new_count == old_count) or ((new_time - old_time) < h['delta_t']):
+        time.sleep(0.05)
+        new_time = time.time()
+        new_count = h['count_in']
+        
+      speed = abs( (new_count-old_count)/(new_time-old_time) * h['speed_scale'])
+      h['speed'] = speed
+      
+      count_out += int((1.0 + speed)*(new_count - old_count))
+
+      h['count_out'] = count_out
+      old_time = new_time
+      old_count = new_count
+
+
+except KeyboardInterrupt:
+    raise SystemExit
