1f5686360a4a9a5d095f0335011e81fdf85f7551
Marc Wintermantel <marc.wintermantel@gmail.com>
2021-03-14 14:43:11 +0000

    added cycle_jog to jog through programs forth and back

---
+++ cycle_jog.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import hal, time
+h = hal.component("cycle_jog")
+h.newpin("active", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("count_in", hal.HAL_S32, hal.HAL_IN)
+h.newpin("delta_t", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("speed_scale", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("speed", hal.HAL_FLOAT, hal.HAL_OUT)
+h.ready()
+
+speed = 0.0
+old_count = h['count_in']
+new_count = old_count
+old_time = time.time()
+new_time = old_time
+count_out = old_count
+
+try:
+    #wait delta_t is set to avoid division by 0
+    while h['delta_t'] == 0.0:
+        time.sleep(0.1)
+
+    #main while-loop
+    while 1:
+      new_time = time.time()
+      new_count = h['count_in']
+      while (new_time - old_time) < h['delta_t']:
+        time.sleep(0.05)
+        new_time = time.time()
+        new_count = h['count_in']
+        
+      speed = (new_count-old_count)/(new_time-old_time) * h['speed_scale']
+      
+      if speed > 0.0:
+          speed = min(speed, 1.0)
+      else:
+          speed = max(speed, -1.0)
+
+      if h['active']:
+          h['speed'] = speed
+      else:
+          h['speed'] = 1.0
+
+      old_time = new_time
+      old_count = new_count
+
+
+except KeyboardInterrupt:
+    raise SystemExit
--- program_run.py
+++ program_run.py
@@ -8,42 +8,57 @@ h.newpin("is_paused", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("is_running", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("auto_toggle", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("single_toggle", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("jog_toggle", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("program_mode", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("run", hal.HAL_BIT, hal.HAL_OUT)
 h.newpin("step", hal.HAL_BIT, hal.HAL_OUT)
 h.newpin("resume", hal.HAL_BIT, hal.HAL_OUT)
 h.newpin("pause", hal.HAL_BIT, hal.HAL_OUT)
+h.newpin("cycle_jog", hal.HAL_BIT, hal.HAL_OUT)
 h.ready()
 
 
 try:
     while 1:
-        if (h['cycle_button'] and h['is_idle'] and h['auto_toggle'] and h['program_mode']):
+        if (h['cycle_button'] and h['is_idle'] and h['auto_toggle'] and h['program_mode']): #start program
             h['step'] = 0
             h['resume'] = 0
             h['run'] = 1
-        elif (h['cycle_button'] and h['single_toggle'] and h['program_mode']):
+        elif (h['cycle_button'] and h['single_toggle'] and h['program_mode']): #start program in single step mode
             h['run'] = 0
             h['resume'] = 0
             h['step'] = 1
-        elif (h['cycle_button'] and h['is_paused'] and h['auto_toggle'] and h['program_mode']):
+        elif (h['cycle_button'] and h['is_paused'] and h['auto_toggle'] and h['program_mode']): #restart program after pause
             h['step'] = 0
             h['run'] = 0
             h['resume'] = 1
-        if (not h['is_paused'] and h['single_toggle'] and h['program_mode']):
+        elif (not h['is_paused'] and h['single_toggle'] and h['program_mode']): #switch to single step 
             h['step'] = 0
             h['resume'] = 0
             h['run'] = 0
             h['pause'] = 1
-        if (h['pause_button'] and h['program_mode']):
+        elif (h['pause_button'] and h['program_mode']): #pause program
             h['step'] = 0
             h['resume'] = 0
             h['run'] = 0
             h['pause'] = 1
+        elif (h['cycle_button'] and h['is_idle'] and h['jog_toggle'] and h['program_mode']): #start program
+            h['step'] = 0
+            h['resume'] = 0
+            h['run'] = 1
+            h['cycle_jog'] = True
+        elif (h['cycle_button'] and h['is_paused'] and h['jog_toggle'] and h['program_mode']): #restart program after pause
+            h['step'] = 0
+            h['run'] = 0
+            h['resume'] = 1
+            h['cycle_jog'] = True
+        elif (h['is_running'] and h['jog_toggle'] and h['program_mode']): #restart program after pause
+            h['cycle_jog'] = True
         else:
             h['run'] = 0
             h['step'] = 0
             h['resume'] = 0
             h['pause'] = 0
+            h['cycle_jog'] = False
 except KeyboardInterrupt:
     raise SystemExit
