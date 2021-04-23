775bd4bdf5c9c55d810d6ff0e8be683c00194027
Marc Wintermantel <marc.wintermantel@gmail.com>
2021-04-23 10:41:59 +0200

    more lubrication stuff

--- lubrication.py
+++ lubrication.py
@@ -2,45 +2,59 @@
 import hal
 import time
 h = hal.component("lubrication")
-h.newparam("lube_command", hal.HAL_BIT, hal.HAL_RW)
-h.newpin("lube_pressure", hal.HAL_BIT, hal.HAL_IN)
+h.newparam("lub_command", hal.HAL_BIT, hal.HAL_RW)
+h.newparam("lub_cycle_time", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("paused", hal.HAL_BIT, hal.HAL_RW)
+h.newparam("lub_interval_time", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("lub_interval_timer", hal.HAL_FLOAT, hal.HAL_RO)
+h.newparam("joint0_lub_dist", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("joint1_lub_dist", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("joint2_lub_dist", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("joint0_lub_hubs", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("joint1_lub_hubs", hal.HAL_FLOAT, hal.HAL_RW)
+h.newparam("joint2_lub_hubs", hal.HAL_FLOAT, hal.HAL_RW)
+h.newpin("lub_pressure", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("joint0_integ", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("joint1_integ", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("joint2_integ", hal.HAL_FLOAT, hal.HAL_IN)
-h.newparam("lube_time", hal.HAL_FLOAT, hal.HAL_RW)
-h.newpin("lube_pump", hal.HAL_BIT, hal.HAL_OUT)
+h.newpin("joint0_vel", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("joint1_vel", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("joint2_vel", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("lub_pump", hal.HAL_BIT, hal.HAL_OUT)
 h.newpin("integ_reset", hal.HAL_BIT, hal.HAL_OUT)
-h.newparam("paused", hal.HAL_BIT, hal.HAL_RW)
+
+
 h.ready()
 
-h.lube_command = True
+h.lub_command = True
 start_time = -1.0
-lube_timer = time.time()
+lub_timer = time.time()
 
 try:
     while 1:
         h.integ_reset = False
+        h.lub_interval_timer = time.time() - lub_timer
 
-        if h.joint0_integ > 100 or h.joint1_integ > 100 or h.joint2_integ > 100:
-            h.lube_command = True
+        if h.joint0_integ > h.joint0_lub_dist or h.joint1_integ > h.joint1_lub_dist or h.joint2_integ > h.joint2_lub_dist:
+            h.lub_command = True
 
-        if time.time() - lube_timer > 180:
-            h.lube_command = True
+        if time.time() - lub_timer > h.lub_interval_time:
+            h.lub_command = True
 
-        if h.lube_command:
-            h.lube_pump = True
+        if h.lub_command:
+            h.lub_pump = True
 
-        if h.lube_command and h.lube_pressure and start_time < 0.0:
+        if h.lub_command and h.lub_pressure and start_time < 0.0:
             start_time = time.time()
 
-        if h.lube_pressure and time.time() - start_time > 15.0:
+        if h.lub_pressure and time.time() - start_time > h.lub_cycle_time:
             print(time.time())
             print(start_time)
-            h.lube_command = False
-            h.lube_pump = False
+            h.lub_command = False
+            h.lub_pump = False
             start_time = -1.0
             h.integ_reset = True
-            lube_timer = time.time()
+            lub_timer = time.time()
         
 
 
