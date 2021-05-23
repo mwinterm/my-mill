5d66ae901e295a37b81cb9dcf5b7c1c3116e8f01
Marc Wintermantel <marc.wintermantel@gmail.com>
2021-05-24 01:22:58 +0200

    fixed debug.py and started cleaning up lubrication.py

---
+++ debug.py
@@ -0,0 +1,29 @@
+#debug.py
+
+class Debug(object):
+    def __init__(self, name="Debug", value = -1):
+        self.__name = name
+        self.__level = value
+        self.__changed = True
+    
+    @property
+    def level(self):
+        return self.__level
+    
+    @level.setter
+    def level(self, value):
+        if self.__level != value:
+            self.__level = value
+            self.__changed = True
+
+    def __str__(self):
+        if self.__changed:
+            self.__changed = False
+            return self.__name + ': ' + str(self.__level)
+        else:
+            return ""
+
+    def msg(self):
+        if self.__changed:
+            self.__changed = False
+            print(self.__name + ': ' + str(self.__level))
--- lubrication.py
+++ lubrication.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import hal
 import time
+import linuxcnc
+from linuxcnc_timer import Timer
+from debug import Debug
+
+
+c = linuxcnc.command()
+
+# c.error_msg('Error')
+# c.text_msg('Text')
+# c.display_msg('Display')
 
 h = hal.component("lubrication")
 h.newparam("lub_command", hal.HAL_BIT, hal.HAL_RW)
@@ -17,9 +27,8 @@ h.newparam("joint2_lub_dist", hal.HAL_FLOAT, hal.HAL_RW)
 h.newparam("joint0_lub_hubs", hal.HAL_FLOAT, hal.HAL_RW)
 h.newparam("joint1_lub_hubs", hal.HAL_FLOAT, hal.HAL_RW)
 h.newparam("joint2_lub_hubs", hal.HAL_FLOAT, hal.HAL_RW)
-h.newparam("debug", hal.HAL_S32, hal.HAL_RW)
 
-
+h.newpin("machine_is_on", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("lub_pressure", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("joint0_integ", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("joint1_integ", hal.HAL_FLOAT, hal.HAL_IN)
@@ -27,12 +36,16 @@ h.newpin("joint2_integ", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("joint0_vel", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("joint1_vel", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("joint2_vel", hal.HAL_FLOAT, hal.HAL_IN)
+
 h.newpin("lub_pump", hal.HAL_BIT, hal.HAL_OUT)
 h.newpin("integ_reset", hal.HAL_BIT, hal.HAL_OUT)
-h.newpin("machine_is_on", hal.HAL_BIT, hal.HAL_IN)
 
 
 h.ready()
+
+while not h.machine_is_on:
+    time.sleep(0.1)
+
 
 #initialize
 first = True
@@ -49,14 +62,24 @@ joint1_hubs = 0
 joint2_hubs = 0
 
 
+
+lub_cycle_timer = Timer(h.lub_cycle_time)
+lub_debug = Debug("lubrication debug")
+lub_debug.level = 100
+
 try:
     while 1:
+        lub_debug.msg()
         if not h.machine_is_on:
-            h.debug = 1
+            
             if first:
+                lub_debug.level = 1
+                lub_debug.msg()
                 # has to be set here as ini variables might not be read yet earlier on
                 num_cycles_start = h.num_cycles
             else: 
+                lub_debug.level = 2
+                lub_debug.msg()
                 h.num_cycles = num_cycles_start
 
             pressure_time = -1.0
@@ -70,7 +93,8 @@ try:
             joint1_hubs = 0
             joint2_hubs = 0
         else:
-            h.debug = 2
+            lub_debug.level = 3
+            lub_debug.msg()
             first = False #mark that it is not the first run ever
 
             h.integ_reset = False
@@ -78,8 +102,9 @@ try:
 
             #do initial lubrications at startup or after estop
             if h.num_cycles < 0:
-                if h.num_cycles == num_cycles_start or time.time() - lub_timer > h.start_cycles_pause:
-                    h.lub_command = True
+                lub_debug.level = 4
+                lub_debug.msg()
+                h.lub_command = True
 
             #do distance traveled based lubrication
             if h.joint0_integ > h.joint0_lub_dist or h.joint1_integ > h.joint1_lub_dist or h.joint2_integ > h.joint2_lub_dist:
@@ -108,24 +133,40 @@ try:
 
             #execute lubrication
             if h.lub_command & h.paused:
+                lub_debug.level = 5
+                lub_debug.msg()
                 h.lub_pump = False
                 if paused_timer < 0:
                     paused_timer = time.time()
                 elif time.time() - paused_timer > h.lub_warning_interval:
                     print("Warning: Lubrication is paused")
+                    c.text_msg("Warning: Lubrication is paused")
                     paused_timer = time.time()
             elif h.lub_command: 
+                lub_debug.level = 6
+                lub_debug.msg()
                 h.lub_pump = True
+                pressure_delay_time = time.time()
 
-            #start the timer for lubricatoin based on pressure
-            if h.lub_command and h.lub_pressure and pressure_time < 0.0:
-                pressure_time = time.time()
+            #check if it takes too long to build pressure
+            if h.lub_command and (not h.lub_pressure) and (time.time() - pressure_delay_time > 3.0):
+                lub_debug.level = 7
+                lub_debug.msg()
+                c.error_msg("Lubrication not working, no pressure building up.")
+                pressure_delay_time = time.time() + 30.0
+
+            #start the timer for lubrication based on pressure
+            if h.lub_command and h.lub_pressure:
+                lub_debug.level = 8
+                lub_debug.msg()
+                lub_cycle_timer.start()
 
             #end the lub cycle
-            if h.lub_pressure and h.lub_command and time.time() - pressure_time > h.lub_cycle_time:
+            if h.lub_pressure and h.lub_command and not lub_cycle_timer():
+                lub_debug.level = 9
+                lub_debug.msg()
                 h.lub_command = False
                 h.lub_pump = False
-                pressure_time = -1.0
                 h.integ_reset = True
                 lub_timer = time.time()
                 h.num_cycles +=1
